3e5aebf466c751ff2309f57cdb887554fbf43e13
Kumar Shridhar <shridhar.stark@gmail.com>
2018-09-11 16:05:03 +0200

    The model is now Bayesian model

--- model.py
+++ model.py
@@ -1,27 +1,44 @@
-import torch
 import torch.nn as nn
 import torch.nn.init as init
+from utils.BBBlayers import BBBConv2d
 
-
-class Net(nn.Module):
-    def __init__(self, upscale_factor):
-        super(Net, self).__init__()
+class BBBLeNet(nn.Module):
+    def __init__(self, upscale_factor ):
+        super(BBBLeNet, self).__init__()
 
         self.relu = nn.ReLU()
-        self.conv1 = nn.Conv2d(1, 64, (5, 5), (1, 1), (2, 2))
-        self.conv2 = nn.Conv2d(64, 64, (3, 3), (1, 1), (1, 1))
-        self.conv3 = nn.Conv2d(64, 32, (3, 3), (1, 1), (1, 1))
-        self.conv4 = nn.Conv2d(32, upscale_factor ** 2, (3, 3), (1, 1), (1, 1))
+        self.conv1 = BBBConv2d(1, 64, (5, 5), (1, 1), (2, 2))
+        self.conv2 = BBBConv2d(64, 64, (3, 3), (1, 1), (1, 1))
+        self.conv3 = BBBConv2d(64, 32, (3, 3), (1, 1), (1, 1))
+        self.conv4 = BBBConv2d(32, upscale_factor ** 2, (3, 3), (1, 1), (1, 1))
         self.pixel_shuffle = nn.PixelShuffle(upscale_factor)
+
+        layers = [self.conv1, self.relu, self.conv2, self.relu, self.conv3,
+                  self.relu, self.conv4, self.relu, self.pixel_shuffle]
+
+        self.layers = nn.ModuleList(layers)
+
 
         self._initialize_weights()
 
-    def forward(self, x):
-        x = self.relu(self.conv1(x))
-        x = self.relu(self.conv2(x))
-        x = self.relu(self.conv3(x))
-        x = self.pixel_shuffle(self.conv4(x))
-        return x
+
+    def probforward(self, x):
+        'Forward pass with Bayesian weights'
+        kl = 0
+        for layer in self.layers:
+            if hasattr(layer, 'convprobforward') and callable(layer.convprobforward):
+                x, _kl, = layer.convprobforward(x)
+                kl += _kl
+
+            elif hasattr(layer, 'fcprobforward') and callable(layer.fcprobforward):
+                x, _kl, = layer.fcprobforward(x)
+                kl += _kl
+            else:
+                x = layer(x)
+        logits = x
+        print('logits', logits)
+        return logits, kl
+
 
     def _initialize_weights(self):
         init.orthogonal_(self.conv1.weight, init.calculate_gain('relu'))
